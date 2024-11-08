7508659f0f49b10b2fe336396ce6b3e51686c7de
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-11-08 15:40:16 +1300

    Completed pick your own adventure app

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI(storyBrain.storyIndex)
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        let nextStoryIndex = storyBrain.nextStory(userAnswer)
+        
+        updateUI(nextStoryIndex)
+    }
+    
+    
+    func updateUI(_ storyIndex: Int) {
+        let newStory = storyBrain.stories[storyIndex]
+        storyLabel.text = newStory.title
+        choice1Button.setTitle(newStory.choice1, for: .normal)
+        choice2Button.setTitle(newStory.choice2, for: .normal)
+    }
 }
 
